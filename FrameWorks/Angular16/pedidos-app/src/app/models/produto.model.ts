a6e6a77e93b63d02ea7fbd44db7324b7a12bda50
Laizinhavivi <vitorialaiza605@gmail.com>
2025-05-28 10:25:51 -0300

    28.05.25

---
+++ FrameWorks/Angular16/pedidos-app/src/app/models/produto.model.ts
@@ -0,0 +1,7 @@
+export class Produto{
+  //construtor abreviado da classe, facilita a criação de classe, colocando os atributos dentro do construtor
+  constructor(
+    public id:number,
+     public nome:string,
+     public preco:number){}
+}
---
+++ FrameWorks/Angular16/pedidos-app/src/app/section/pedido-form/pedido-form.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { Cliente } from 'src/app/models/cliente.model';
+import { Pedido } from 'src/app/models/pedido.model';
+import { Produto } from 'src/app/models/produto.model';
+import { DadosService } from 'src/app/services/dados.service';
+
+@Component({
+  selector: 'app-pedido-form',
+  templateUrl: './pedido-form.component.html',
+  styleUrls: ['./pedido-form.component.scss']
+})
+export class PedidoFormComponent {
+  clienteSelecionadoId = "";
+  clientes:Cliente[];
+  produtos:Produto[];
+  produtosSelecionados:string[] =[]
+  desconto = 0;
+
+constructor(private dadosService: DadosService){
+  this.clientes = dadosService.getClientes();
+  this.produtos = dadosService.getProdutos();
+}
+//métodos
+mudarProduto(id:string){
+  const index = this.produtosSelecionados.indexOf(id);
+  if(index>=0){
+    this.produtosSelecionados.splice(index,1);
+  }else{
+    this.produtosSelecionados.push(id);
+  }
+}
+
+salvarPedido(){
+  const Cliente = this.clientes.find(
+  c => c.id.toString() === this.clienteSelecionadoId);
+  const produtos = this.produtos.filter(
+    p=>this.produtosSelecionados.includes(p.id.toString())
+  );
+  if (!Cliente || produtos.length === 0 ){
+    alert("Selecione um cliente e pelo menos um produto!!!");
+    return;
+  }else{
+    const pedido = new Pedido(
+      Date.now(),
+      Cliente,
+      produtos,
+      this.desconto,
+    );
+    this.dadosService.addPedido(pedido);
+    this.clienteSelecionadoId = "";
+    this.produtosSelecionados = [];
+    this.desconto = 0;
+    alert("Pedido Registrado com Sucesso!!!")
+  }}}

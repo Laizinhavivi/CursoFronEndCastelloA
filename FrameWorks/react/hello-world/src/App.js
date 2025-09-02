d0c70e06d8216aa3b8b24c33e89088d8a9888a22
Laizinhavivi <vitorialaiza605@gmail.com>
2025-09-02 16:59:02 -0300

    02/09/25

--- FrameWorks/react/hello-world/src/App.js
+++ FrameWorks/react/hello-world/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <p>Minha Primeira Página em React</p>
     </div>
   );
 }
--- FrameWorks/react/to-do-list/src/App.js
+++ FrameWorks/react/to-do-list/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import TodoForm from "./componentes/TodoForms";
+import TodoList from "./componentes/TodoList";
+import "./App.css"
+//Declaração do Componente
+const App = () =>{
+  //Controle de Estado
+  const [task,setTasks] = useState([]);
+  // Função para adicionar Tarefa
+  const addTask = (task) => {
+    //adiciona uma nova tarefa ao vetor de tarefas
+    setTasks([...task, task]);
+  }
+  //Função para remover Tarefa
+  const removeTask = (index) =>{
+    // buscar a tarefa pelo índice e remove
+    setTasks(task.filter((_,i)=> i !==index));
+    // Cria um novo vetor sem a tarefa que quero remover
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      <h1>Lista de Tarefas</h1>
+      <TodoForm addTask = {addTask}/>
+      <TodoList tasks = {task} removeTask = {removeTask}/>
     </div>
-  );
-}
+  )
+};
 
 export default App;
+//componente principal da minha aplicação
